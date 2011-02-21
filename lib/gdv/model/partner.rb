552452b87c06858836e43597e7f7a98d407013d9
Georg Ledermann <mail@georg-ledermann.de>
2011-02-21 15:24:57 +0100

    Partner: Added fields for "Kommunikation" and "Bankverbindung"

--- lib/gdv/model/partner.rb
+++ lib/gdv/model/partner.rb
@@ -21,7 +21,35 @@ module GDV::Model
         property :geschlecht, :address, 1, 25
 
         property :kdnr_vu,  :address, 2, 8
+        property :kontonummer_1,    :address, 2, 11
+        property :blz_1,            :address, 2, 12
+        property :kontoinhaber_1,   :address, 2, 13
+        property :komm_typ_1,       :address, 2, 14
+        property :komm_nummer_1,    :address, 2, 15
+        property :komm_typ_2,       :address, 2, 16
+        property :komm_nummer_2,    :address, 2, 17
+        property :komm_typ_3,       :address, 2, 18
+        property :komm_nummer_3,    :address, 2, 19
+        property :komm_typ_4,       :address, 2, 20
+        property :komm_nummer_4,    :address, 2, 21
+        
+        property :komm_typ_5,       :address, 3, 8
+        property :komm_nummer_5,    :address, 3, 9
+        property :komm_typ_6,       :address, 3, 10
+        property :komm_nummer_6,    :address, 3, 11
+        property :komm_typ_7,       :address, 3, 12
+        property :komm_nummer_7,    :address, 3, 13
 
+        property :kreditinstitut_1, :address, 4, 9
+        property :kontonummer_2,    :address, 4, 12
+        property :blz_2,            :address, 4, 13
+        property :kontoinhaber_2,   :address, 4, 14
+        property :kreditinstitut_2, :address, 4, 15
+        property :bic_1,            :address, 4, 16
+        property :bic_2,            :address, 4, 17
+        property :iban_1,           :address, 4, 18
+
+        property :iban_2,        :address, 5, 8
         property :geburtsort, :address, 5, 9
     end
 end
